5687d5557ae3407e1dc77683cfbc9749a4e438a5
Zak Saylors <zsaylors11@gmail.com>
2019-09-21 12:56:23 -0600

    need to add quit exception, truckid, and a few other user stories.  Menus working.  Quit error.

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -2,10 +2,70 @@ package com.skilldistillery.foodtrucks;
 
 public class FoodTruck {
 
+	// F I E L D S
 	private static int truckId = 0;
+	private String truckName;
+	private String foodType;
+	private int rating;
 	
-	public FoodTruck(int truckId) {
-		truckId++;
+	
+	// C O N S T R U C T O R S
+	public FoodTruck() {
 	}
+	
+	public FoodTruck(String truckName, String foodType, int rating) {
+		this(0, truckName, foodType, rating);
+	}
+	
+	public FoodTruck(int truckId, String truckName, String foodType, int rating) {
+		this.truckId = truckId;
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.rating = rating;
+	}
+
+	// M E T H O D S
+	public String output() {
+		return getTruckName() + " " + foodType + " " + rating;
+	}
+	
+	public static int getTruckId() {
+		return truckId;
+	}
+
+
+	public static void setTruckId(int truckId) {
+		FoodTruck.truckId = truckId;
+	}
+
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+
+
+	public void setRating(String truckName) {
+		this.rating = rating;
+	}
+	
 	
 }
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -1,34 +1,68 @@
 package com.skilldistillery.foodtrucks;
 
+import java.awt.List;
 import java.util.Scanner;
 
 public class FoodTruckApp {
 	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		FoodTruckApp f = new FoodTruckApp();
-		
-		// Allows user selection and prints menu
-		f.menuSelection(scanner);
-		
-
-		scanner.close();
+		FoodTruckApp app = new FoodTruckApp();
+		FoodTruck[] truck = null;
+		app.run(truck);
 	}
 	
+	public void run(FoodTruck[] truck) {
+		Scanner kb = new Scanner(System.in);
+		menuSelection(kb, truck);
+		kb.close();
+	}
+	
+	// Gets user input via scanner and returns a populated FoodTruck[] array.
+	public FoodTruck[] getUserInput(Scanner kb, FoodTruck[] truck) {
+		// Determines array size.
+		System.out.print("How many food trucks are you adding? ");
+		int numOfTrucks = kb.nextInt();
+		kb.nextLine();  // resolves nextLine errors
+		truck = new FoodTruck[numOfTrucks];
+		
+		// For Loop gets name, food type, and rating
+		for (int i = 0; i < numOfTrucks; i++) {	
+			System.out.println("\n-- Truck " + (i + 1) + " --");
+			System.out.print("Food truck name: ");
+			String truckName = kb.nextLine();
+			System.out.print("Food type: ");
+			String foodType = kb.nextLine();
+			System.out.print("numeric rating (1-5): ");
+			int rating = kb.nextInt();
+			kb.nextLine(); // resolves nextLine errors
+
+			truck[i] = new FoodTruck(truckName, foodType, rating);
+		}
+		System.out.println("\nDone with input.  Returning to menu.");
+		// Updated menu.  Entering 2-4 will now return appropriate values.
+		menuSelection(kb, truck);
+		return truck;
+	}
+
 	// Menu selection and outputs
-	public void menuSelection(Scanner kb) {
+	public void menuSelection(Scanner kb, FoodTruck[] truck) {
 		boolean run = true;
 		while(run) {
 			menu();
 			// String used rather than int, so user entered strings or chars won't break program.
 			String userSelection = kb.next();
 			switch(userSelection) {
-	//			case "1": break;
-	//			case "2": break;
-	//			case "3": break;
-	//			case "4": break;
-				case "5": run = false;
-						  System.out.println("\n~ Ciao! ~");
+				case "1": exception(kb, truck, run);
+						  getUserInput(kb, truck);
+						  break;
+				case "2": viewRatings(kb, truck);
+						  break;
+				case "3": averageRatings(truck);
+						  break;
+				case "4": bestRated(truck);
+						  break;  
+				case "5": System.out.println("~ Ciao! ~");
+						  run = false;
 						  break;
 				default: System.out.println("\nInvalid reponse.  Please pick a number 1 - 5.");
 						 break;
@@ -42,12 +76,83 @@ public class FoodTruckApp {
 					   ("____________________\n")
 				.append(" FOOD TRUCK REVIEWS  \n")
 				.append("_____ - Menu - _____\n")
-			    .append("\n")
 			    .append(" 1. Rate trucks\n")
-			    .append(" 2. See ratings\n")
+			    .append(" 2. See ratings \u2b50\n")
 			    .append(" 3. Average rating\n")
 			    .append(" 4. Best truck\n")
 			    .append(" 5. Quit\n");
 		System.out.println(menu);
+	}
+
+	// CASE 2:   List all existing food trucks.
+	public void viewRatings(Scanner kb, FoodTruck[] truckList) {
+		if (truckList == null) {
+			System.out.println("You have not entered any data in yet!");
+		}
+		else {
+			for (int i = 0; i < truckList.length; i++) {
+				System.out.println(truckList[i].output());
+			}
+		}
+		run(truckList);
+	}
+	
+	// Exception prevents user from accidently erasing list
+	public void exception(Scanner kb, FoodTruck[] truckList, boolean run) {
+		if (truckList != null) {
+			System.out.print("This will reset the food truck list.  Continue?  (Y / N) ");
+			String exceptionResponse = kb.next();
+			if (exceptionResponse.equalsIgnoreCase("Y")) {
+				run = false;
+			}
+			else if (exceptionResponse.equalsIgnoreCase("N")) {
+				System.out.println("Roger! Returning to menu.\n");
+				run(truckList);
+			}
+			else {
+				System.out.println("Invalid response.  Returning to menu.\n");
+				run(truckList);
+				
+			}
+		}
+	}
+	
+	// Case 4:  Returns average rating
+	public void averageRatings(FoodTruck[] truckList) {
+		if (truckList == null) {
+			System.out.println("You have not entered any data in yet!");
+		}
+		else {
+			double sum = 0;
+			for (int i = 0; i < truckList.length; i++) {
+				sum += truckList[i].getRating();
+			}
+			System.out.printf("The average food truck rating is %.2f%n", sum/truckList.length);
+		}
+		run(truckList);
+	}
+	
+	// Case 5:  Returns best rated
+	public void bestRated(FoodTruck[] truckList) {
+		if (truckList == null) {
+			System.out.println("You have not entered any data in yet!");
+		}
+		else {
+			int highestRating = 0;
+			String bestTruck = "";
+			for (int i = 0; i < truckList.length; i++) {
+				if (highestRating < truckList[i].getRating()){
+					highestRating = truckList[i].getRating();
+					bestTruck = truckList[i].output();
+				}
+			} System.out.println(bestTruck);
+			
+		}
+		run(truckList);
+	}
+	
+	// Quits program
+	public void quit() {
+		System.out.println("\n~ Ciao! ~");
 	}
 }
